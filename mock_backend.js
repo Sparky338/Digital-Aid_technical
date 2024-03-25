35affdaa7c2f97abaef97c6ca470fd4d7a79cf0e
Matthew Hutter <matthewjhutter@gmail.com>
2024-03-25 16:21:30 -0700

    backend skeleton created

---
+++ mock_backend.js
@@ -0,0 +1,16 @@
+/* This section would ideally be a database to maintain the inventory between
+days/ running the app. For simplicity, this will consist of the functions that
+would be needed for a backend including storing an inventory, adding to the
+inventory, and ditributing the inventory. */
+
+const inventory = (donationType, qty) => {
+    
+};
+
+const donation = (donorName, donationType, qty) => {
+
+};
+
+const donationDistribution = (donationType, qty) => {
+
+};
