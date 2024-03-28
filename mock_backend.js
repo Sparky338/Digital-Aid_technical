728624f11b52db33b5bd2c1f1d05894ae6bdc100
Matthew Hutter <matthewjhutter@gmail.com>
2024-03-28 13:40:56 -0700

    donation distribution created

--- mock_backend.js
+++ mock_backend.js
@@ -4,48 +4,62 @@ would be needed for a backend including storing an inventory, adding to the
 inventory, and ditributing the inventory. */
 
 // Stores the inventory
-const inventory = {donationType: qty}
+const inventory = { donationType: qty };
 
 // Stores the Donor list
-const donors = {donorName: {
-    donationType: qty
-}}
+const donors = {
+  donorName: {
+    donationType: qty,
+  },
+};
 
 // Stores the donation list
 const donations = [];
 
 // Stores the distribution logs
-const distribution = {
-    donationType: qty,
-    date: Date.now()
-}
+const distribution = [];
 
 const donation = (donorName, donationType, qty) => {
-    // Adds to a log of donors, adds the donation type and quantity to the inventory,
-    // adds to the donation list. Date added to donations on calling donation function.
-    donations.push({
-        name: donorName,
-        type: donationType,
-        quantity: qty,
-        date: Date.now()
-    })
+  // Adds to a log of donors, adds the donation type and quantity to the inventory,
+  // adds to the donation list. Date added to donations on calling donation function.
+  donations.push({
+    name: donorName,
+    type: donationType,
+    quantity: qty,
+    date: Date.now(),
+  });
 
-    if (inventory[donationType]){
-        inventory[donationType] += qty
-    } else {
-        inventory[donationType] = qty
-    }
+  if (inventory[donationType]) {
+    inventory[donationType] += qty;
+  } else {
+    inventory[donationType] = qty;
+  }
 
-    if (donors[donorName][donationType]) {
-        donors[donorName][donationType] += qty;
-    } else {
-        donors[donorName][donationType] = qty;
-    };
-
+  if (donors[donorName][donationType]) {
+    donors[donorName][donationType] += qty;
+  } else {
+    donors[donorName][donationType] = qty;
+  }
 };
 
 const donationDistribution = (donationType, qty) => {
-// If the donation type exists AND the quantity is valid/
-// would not drop below 0, reduces the quantity from inventory.
-// date added on calling donationDistibution function
+  // If the donation type exists AND the quantity is valid/
+  // would not drop below 0, reduces the quantity from inventory.
+  // date added on calling donationDistibution function
+
+  if (inventory[donationType] >= qty) {
+    inventory[donationType] -= qty;
+    distribution.push({
+        type: donationType,
+        quantity: qty,
+        date: Date.now()
+    });
+    return `${donationType} has been distributed ${qty} times.`
+  } else if (inventory[donationType] < qty) {
+        return `There is not enough ${donationType} to ditrubute ${qty} times.
+                There is ${inventory[donationType]} available to distribute.`
+  } else {
+        return `There is no ${donationType} available for distrubution. Please
+                check the inventory for donations available to distribute.`
+  }
 };
