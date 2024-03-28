7ed4e147952cd1d7be05319de41b55bf39bdb60a
Matthew Hutter <matthewjhutter@gmail.com>
2024-03-28 11:46:59 -0700

    created donation list, added logic to donation function

--- mock_backend.js
+++ mock_backend.js
@@ -8,9 +8,11 @@ const inventory = {donationType: qty}
 
 // Stores the Donor list
 const donors = {donorName: {
-    donationType: qty,
-    date: Date.now()
+    donationType: qty
 }}
+
+// Stores the donation list
+const donations = [];
 
 // Stores the distribution logs
 const distribution = {
@@ -19,8 +21,21 @@ const distribution = {
 }
 
 const donation = (donorName, donationType, qty) => {
-// Adds to a log of donors, adds the donation type and quantity to the inventory.
-// date added on calling donation function
+    // Adds to a log of donors, adds the donation type and quantity to the inventory.
+    // date added on calling donation function
+    donations.push({
+        name: donorName,
+        type: donationType,
+        quantity: qty,
+        date: Date.now()
+    })
+
+    if (donors[donorName][donationType]) {
+        donors[donorName][donationType] += qty;
+    } else {
+        donors[donorName][donationType] = qty;
+    };
+
 };
 
 const donationDistribution = (donationType, qty) => {
