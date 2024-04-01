272dd6482375ee0e95e47f200389c2a36a4245eb
Matthew Hutter <matthewjhutter@gmail.com>
2024-03-31 17:51:33 -0700

    homepage display to test

--- digital-aid-seattle/src/components/homepage.js
+++ digital-aid-seattle/src/components/homepage.js
@@ -1,7 +1,12 @@
 import { Link } from "react-router-dom";
 
 const Homepage = () => {
-
+    return (
+        <div>
+            <h1> Best Shelter in the PNW! </h1>
+            
+        </div>
+    );
 }
 
 export default Homepage
