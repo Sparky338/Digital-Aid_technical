bac5b8296196bdbd392d57f168dec1ffb6828542
Matthew Hutter <matthewjhutter@gmail.com>
2024-04-02 11:53:46 -0700

    created a description of the purpose on the page

--- digital-aid-seattle/src/components/homepage.js
+++ digital-aid-seattle/src/components/homepage.js
@@ -1,12 +1,17 @@
 import { Link } from "react-router-dom";
 
 const Homepage = () => {
-    return (
-        <div>
-            <h1> Best Shelter in the PNW! </h1>
-            
-        </div>
-    );
-}
+  return (
+    <div className="mainDiv">
+      <h1 className="title"> Best Shelter in the PNW! </h1>
+      <h2 className="pageDescription">
+        Here is the homepage of the shelter's inventory control page.
+        Please use the links below or on the navigation bar to access
+        the donation registration, distribution, or reports.
+      </h2>
 
-export default Homepage
+    </div>
+  );
+};
+
+export default Homepage;
