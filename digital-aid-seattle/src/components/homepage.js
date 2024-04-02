1adc4af10a6d06af52dd16cb9267c2f40e603baa
Matthew Hutter <matthewjhutter@gmail.com>
2024-04-02 12:04:55 -0700

    created temporary links to future pages

--- digital-aid-seattle/src/components/homepage.js
+++ digital-aid-seattle/src/components/homepage.js
@@ -5,11 +5,25 @@ const Homepage = () => {
     <div className="mainDiv">
       <h1 className="title"> Best Shelter in the PNW! </h1>
       <h2 className="pageDescription">
-        Here is the homepage of the shelter's inventory control page.
-        Please use the links below or on the navigation bar to access
-        the donation registration, distribution, or reports.
+        Here is the homepage of the shelter's inventory control page. Please use
+        the links below or on the navigation bar to access the donation
+        registration, distribution, or reports.
       </h2>
+      <div className="links-list">
+        <Link to="/donation" className="link donation-link">
+            <div className="donation">Donations</div>
+        </Link>
+        <Link to="/distribution" className="link distribution-link">
+            <div className="distribution">Distribution</div>
+        </Link>
+        <Link to="/inventory" className="link inventory-link">
+            <div className="inventory">Inventory</div>
+        </Link>
+        <Link to="/donators" className="link donators-link">
+            <div className="donators">Donators</div>
+        </Link>
 
+      </div>
     </div>
   );
 };
