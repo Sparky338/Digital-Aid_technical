8c69a2bc8744e0c0208410621e3381dd7b272264
Matthew Hutter <matthewjhutter@gmail.com>
2024-04-02 21:44:53 -0700

    created donation form and donation button

--- digital-aid-seattle/src/components/donation.js
+++ digital-aid-seattle/src/components/donation.js
@@ -35,9 +35,52 @@ const Donation = () => {
         <div className="mainDiv">
             <div className="donations">
                 <h1>Donations</h1>
-                <div className="donation-form form">
-                    {/* Donation form goes here */}
+                <div className="donation-container form">
+                    <form className="donation-form" onSubmit={handleSubmit}>
+                        {hasSubmitted && validationErrors.length > 0 && (
+                            <div className="outer-error">
+                                <div className="errorhandling">
+                                    <ul className="errors">
+                                        {validationErrors.map(error => (
+                                            <li className="error-list" key={error}>{error}</li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            </div>
+                        )}
+                        <label className="donation-form-label">
+                            Name
+                            <input
+                                className="donation-form-input"
+                                type="text"
+                                value={name}
+                                onChange={e => setName(e.target.value)}
+                                placeholder="Name"
+                            />
+                        </label>
+                        <label className="donation-form-label">
+                            Donation Type
+                            <input
+                                className="donation-form-input"
+                                type="text"
+                                value={donationType}
+                                onChange={e => setDonationType(e.target.value)}
+                                placeholder="Donation Type"
+                            />
+                        </label>
+                        <label className="donation-form-label">
+                            Quantity
+                            <input
+                                className="donation-form-input"
+                                type="number"
+                                value={qty}
+                                onChange={e => setQty(e.target.value)}
+                                placeholder="Quantity"
+                            />
+                        </label>
+                    </form>
                 </div>
+                <button className="button donation-button" onClick={handleSubmit}>Donate!</button>
             </div>
         </div>
     )
