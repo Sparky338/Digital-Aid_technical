a4dfde5fbebe6057d6688e93d57a1b045c9813bd
Matthew Hutter <matthewjhutter@gmail.com>
2024-04-02 19:22:35 -0700

    skeleton created for donations, routes created

--- digital-aid-seattle/src/App.js
+++ digital-aid-seattle/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Homepage from './components/homepage';
+import Donation from './components/donation';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path = '/' exact = {true} element = {<Homepage />} />
+        <Route path = '/donation' element = {<Donation />} />
+        {/* <Route path = '/distribution' element = {<Distribution />} /> */}
+        {/* <Route path = '/inventory' element = {<Inventory />} /> */}
+        {/* <Route path = '/donators' element = {<Donators />} /> */}
       </Routes>
     </BrowserRouter>
   );
---
+++ digital-aid-seattle/src/components/donation.js
@@ -0,0 +1,12 @@
+const Donation = () => {
+
+    return (
+        <div className="mainDiv">
+            <div className="donations">
+                <h1>Donations</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Donation;
