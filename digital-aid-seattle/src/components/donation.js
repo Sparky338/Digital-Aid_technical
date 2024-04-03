26b390852ab8574832c26ce0c67a7393719558a9
Matthew Hutter <matthewjhutter@gmail.com>
2024-04-02 19:43:56 -0700

    set up error handling and inputs for donations

--- digital-aid-seattle/src/components/donation.js
+++ digital-aid-seattle/src/components/donation.js
@@ -1,9 +1,30 @@
+import { useEffect, useState } from "react";
+
 const Donation = () => {
+
+    const [name, setName] = useState("");
+    const [donationType, setDonationType] = useState("");
+    const [qty, setQty] = useState(0);
+
+    const [validationErrors, setValidationErrors] = useState([]);
+
+    useEffect(() => {
+        const errors = [];
+
+        if (!donationType) errors.push("A donation type must be specified");
+        if (!qty) errors.push("A quantity must be entered");
+        if (isNaN(qty)) errors.push("Quantity must be a numerical value greater than 0");
+
+        setValidationErrors(errors);
+    }, [donationType, qty])
 
     return (
         <div className="mainDiv">
             <div className="donations">
                 <h1>Donations</h1>
+                <div className="donation-form form">
+                    {/* Donation form goes here */}
+                </div>
             </div>
         </div>
     )
--- mock_backend.js
+++ mock_backend.js
@@ -1,7 +1,7 @@
 /* This section would ideally be a database to maintain the inventory between
 days/ running the app. For simplicity, this will consist of the functions that
 would be needed for a backend including storing an inventory, adding to the
-inventory, and ditributing the inventory. */
+inventory, and distributing the inventory. */
 
 // Stores the inventory
 const inventory = { /*donationType: qty*/ };
