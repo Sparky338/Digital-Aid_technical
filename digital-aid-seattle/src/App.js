d63b9daf8e19db04bcd62f96866cb9c47238bd7b
Matthew Hutter <matthewjhutter@gmail.com>
2024-04-03 11:42:26 -0700

    distribution skeleton created, connected route

--- digital-aid-seattle/src/App.js
+++ digital-aid-seattle/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Homepage from './components/homepage';
 import Donation from './components/donation';
+import Distribution from './components/distribution';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Routes>
         <Route path = '/' exact = {true} element = {<Homepage />} />
         <Route path = '/donation' element = {<Donation />} />
-        {/* <Route path = '/distribution' element = {<Distribution />} /> */}
+        <Route path = '/distribution' element = {<Distribution />} />
         {/* <Route path = '/inventory' element = {<Inventory />} /> */}
         {/* <Route path = '/donators' element = {<Donators />} /> */}
       </Routes>
---
+++ digital-aid-seattle/src/components/distribution.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+import { donationDistribution } from "../mock_backend";
+
+const Distribution = () => {
+
+    return (
+        <div className="mainDiv">
+            <div className="distributions">
+                <h1>Distributions</h1>
+
+            </div>
+        </div>
+    )
+}
+
+export default Distribution
