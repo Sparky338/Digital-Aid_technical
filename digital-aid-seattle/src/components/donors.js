29dfc2b14bd7041b6fbf7fe0da8b7309c7a2a779
Matthew Hutter <matthewjhutter@gmail.com>
2024-04-09 15:52:57 -0700

    desired layout created, need to adjust local storage for donors to display properly

--- digital-aid-seattle/src/components/donors.js
+++ digital-aid-seattle/src/components/donors.js
@@ -1,12 +1,39 @@
 const Donors = () => {
+    const donors = JSON.parse(localStorage.getItem('donors'));
+
     return (
         <div className="mainDiv">
-            <div className="donors">
-                <h1>Donors</h1>
+        <div className="donors">
+            <h1>Donors</h1>
+            <div className="donors-display-table">
+            <tbody className="table-body" border="0" cellSpacing="0">
+                <thead className="table-header">
+                <tr>
+                    <th className="donor-name">Donor Name</th>
+                    <th className="donor-donation-type">Donation Type</th>
+                    <th className="donor-donation-qty">Quantity</th>
+                </tr>
+                </thead>
+                {donors && Object.entries(donors).map((donor, i) => {
+                    return (
+                    <tr className="donor-name-object" key={i}>
+                        {donor && Object.entries(donor).map((item, i) => {
+                            return (
+                                <td className="donors-item-name">{item}</td>
+                                // <td className="donors-item-quantity">
+                                // {donor[item[0]]}
+                                // </td>
 
+                            )
+                        })}
+                    </tr>
+                    );
+                })}
+            </tbody>
             </div>
         </div>
-    )
-}
+        </div>
+    );
+};
 
 export default Donors;
--- digital-aid-seattle/src/components/inventory.js
+++ digital-aid-seattle/src/components/inventory.js
@@ -6,10 +6,10 @@ const Inventory = () => {
       <h1>Inventory</h1>
       <div className="inventory-display-table">
         <tbody className="table-body" border="0" cellSpacing="0">
-          <thead className="table=header">
+          <thead className="table-header">
             <tr>
               <th className="inventory-donation-type">Donation Type</th>
-              <th className="inventory-donation-type">Quantity</th>
+              <th className="inventory-donation-qty">Quantity</th>
             </tr>
           </thead>
           {inventory &&
