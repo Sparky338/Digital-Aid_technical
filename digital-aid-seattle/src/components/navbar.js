b6531dc99b835d7737b0055578395fe1f3a84682
Matthew Hutter <matthewjhutter@gmail.com>
2024-04-10 11:19:06 -0700

    Links added to navbar, currently not displaying

--- digital-aid-seattle/src/components/navbar.js
+++ digital-aid-seattle/src/components/navbar.js
@@ -1,10 +1,29 @@
-const NavBar = () => {
+import { NavLink } from "react-router-dom";
 
-    return (
-        <div className="main-nav-bar">
-            <h1>NavBar placeholder</h1>
-        </div>
-    )
+const NavBar = () => {
+  return (
+    <div className="main-nav-bar">
+      <h1>NavBar placeholder</h1>
+      <div className="logo"></div>
+      <div className="links">
+        <NavLink className={"home-link nav-link"} to="/" exact={true}>
+          Home
+        </NavLink>
+        <NavLink className={"donation-link nav-link"} to="/donation">
+          Donation
+        </NavLink>
+        <NavLink className={"distribution-link nav-link"} to="/distribution">
+          Distribution
+        </NavLink>
+        <NavLink className={"inventory-link nav-link"} to="/inventory">
+          Inventory
+        </NavLink>
+        <NavLink className={"donors-link nav-link"} to="/donors">
+          Donors
+        </NavLink>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
