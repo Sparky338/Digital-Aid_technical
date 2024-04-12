7a78a9d698d5b63a347512b27cb1505fe30e0c3d
Matthew Hutter <matthewjhutter@gmail.com>
2024-04-11 17:10:48 -0700

    styled inventory page

--- digital-aid-seattle/src/components/inventory.js
+++ digital-aid-seattle/src/components/inventory.js
@@ -9,9 +9,9 @@ const Inventory = () => {
       <div className="inventory-display-table">
         <tbody className="table-body" border="0" cellSpacing="0">
           <thead className="table-header">
-            <tr>
+            <tr className="inventory-header">
               <th className="inventory-donation-type">Donation Type</th>
-              <th className="spacer" width="20px">&nbsp;</th>
+              {/* <th className="spacer" width="20px">&nbsp;</th> */}
               <th className="inventory-donation-qty">Quantity</th>
             </tr>
           </thead>
